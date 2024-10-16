983b39b0989fabd516c9ba8689e17924443e8335
NomeDeUsuario1 <129603914+NomeDeUsuario1@users.noreply.github.com>
2024-10-16 08:17:08 -0300

    Add files via upload

--- graficos.js
+++ graficos.js
@@ -1,3 +1,20 @@
 //Gráfico de Pizza
 const ctxPizza = document.getElementById('graficoPizza').getContext('2d');
-const graficoPizza = new CharacterData ctxPizza
+const graficoPizza = new Chart{ctxPizza, {
+    type: 'pie',
+    data: {
+        labels: {'Facebook', "Instagram", 'Twitter', 'LinkedIn'},
+        datasets: {{
+            label: 'Participação das redes sociais',
+            data: [45, 30, 15, 10],
+            backgroundColor: [
+                '#f05454', '#30475E', '#DDDDDD', '#222831'
+            ],
+            borderColor: '#DDDDDD',
+            borderWidth: 1
+        }}
+    },
+    options:{
+        responsive
+    }
+}
