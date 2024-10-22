538d604e7d8d8cb0a64314ef9d15fadbf3e15e8e
NomeDeUsuario1 <129603914+NomeDeUsuario1@users.noreply.github.com>
2024-10-22 09:10:47 -0300

    Add files via upload

--- graficos.js
+++ graficos.js
@@ -1,6 +1,6 @@
 //Gráfico de Pizza
 const ctxPizza = document.getElementById('graficoPizza').getContext('2d');
-const graficoPizza = new Chart{ctxPizza, {
+const graficoPizza = new Chart(ctxPizza, {
     type: 'pie',
     data: {
         labels: {'Facebook', "Instagram", 'Twitter', 'LinkedIn'},
@@ -15,6 +15,48 @@ const graficoPizza = new Chart{ctxPizza, {
         }}
     },
     options:{
-        responsive
+        responsive: true,
+        plugins: {
+            legend: {
+                position: 'bottom',
+            },
+            title: {
+                display: true,
+                text: 'Participação nas Redes Sociais'
+            }
+        } 
+        
     }
-}
+});
+//Gráfico de barras
+const ctxBarra = document.getElementById('graficoBarra').getContext('2d');
+const graficoBarra = new Chart(ctxBarra, {
+    type: 'bar',
+    data: {
+        labels: ['Facebook', 'Instagram', 'Twitter', 'LinkedIn'],
+        datasets: [{
+            label: 'Usuários ativos (em milhões)',
+            data: [2800, 1500, 330, 810],
+            backgroundColor: '#F05454',
+            borderColor: '#30475E',
+            borderWidth: 1
+        }]
+    },
+    options: {
+        responsive: true,
+        plugins: {
+            legend: {
+                position: 'top',
+            },
+            title: {
+                display: true,
+                text: 'Usuários Ativos nas Redes Sociais (milhões)'
+            }
+        },
+        scales: {
+            y: {
+                beginAtZero: true
+            }
+        }
+    }
+});
