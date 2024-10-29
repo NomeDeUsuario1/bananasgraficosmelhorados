5ca4f8c154a156798c7ec75726e4c52f8ee0f7ba
NomeDeUsuario1 <129603914+NomeDeUsuario1@users.noreply.github.com>
2024-10-29 17:53:15 -0300

    Update graficos.js

--- graficos.js
+++ graficos.js
@@ -28,36 +28,6 @@ const graficoPizza = new Chart(ctxPizza, {
         
     }
 });
-//Gráfico de Pizza 2
-const ctxPizza = document.getElementById('graficoPizza').getContext('2d');
-const graficoPizza = new Chart(ctxPizza, {
-    type: 'pie',
-    data: {
-        labels: ['Facebook' , 'Instagram' , 'Twitter' , 'LinkedIn' ],
-        datasets: [{
-            label: 'Participação das redes sociais',
-            data: [45, 30, 15, 10],
-            backgroundColor: [
-                '#f05454', '#30475E', '#DDDDDD', '#222831'
-            ],
-            borderColor: '#DDDDDD',
-            borderWidth: 1
-        }]
-    },
-    options:{
-        responsive: true,
-        plugins: {
-            legend: {
-                position: 'bottom',
-            },
-            title: {
-                display: true,
-                text: 'Participação nas Redes Sociais'
-            }
-        } 
-        
-    }
-});
 //Gráfico de barras
 const ctxBarra = document.getElementById('graficoBarra').getContext('2d');
 const graficoBarra = new Chart(ctxBarra, {
